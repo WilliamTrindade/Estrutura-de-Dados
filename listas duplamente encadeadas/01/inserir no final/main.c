f8d0fcc066590888dde4f10498f0660b35fbbe55
William Trindade <williamtrindade777@gmail.com>
2018-08-31 17:26:16 -0300

    Create main.c

---
+++ listas duplamente encadeadas/01/inserir no final/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lib.h"
+main(){
+	Lista *l;
+	l = NULL;
+	l = insere(l, 1);
+	l = insere(l, 2);
+	l = insere(l, 6);
+	l = insere(l, 10);
+	l = insere(l, 6);
+	
+	imprime(l);
+}
