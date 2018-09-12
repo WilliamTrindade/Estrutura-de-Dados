176ae84b0cb58659fc5a8e605b9818dbd47480f2
William Trindade <williamtrindade777@gmail.com>
2018-09-12 16:23:10 -0300

    Create main.c

---
+++ recursividade/01/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int recursiva(int a);
+
+main(){
+	int x = 1, resultado;
+	resultado = recursiva(x);
+	printf("%d - ", resultado);
+	getch();
+}
+
+int recursiva(int a){
+	if (a > 3){
+		return a;
+	}else{
+		a++;
+		a = recursiva(a);
+	}
+	return a;
+}
