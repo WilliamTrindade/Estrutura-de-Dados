88a1e7924d6ec7e5a7ce66d4090aeb53866cffa0
William Trindade <williamtrindade777@gmail.com>
2018-09-14 16:35:32 -0300

    Create lib.h

---
+++ arvore binaria de busca/01/imprimir/em-ordem/lib.h
@@ -0,0 +1,45 @@
+struct x{
+	int v;
+	struct x *esq;
+	struct x *dir;
+};
+
+typedef struct x arv;
+
+arv *insere(arv *r, int c){
+	arv *novo = (arv*) malloc(sizeof(arv));
+	novo->v = c;
+	novo->esq = NULL;
+	novo->dir = NULL;
+	
+	if(r == NULL){
+		r = novo;
+	}else{
+		arv *aux = r, *ult;
+		
+		while (aux != NULL){
+			ult = aux;
+			//verifica se vai para a esquerda ou direita
+			if(c < aux->v)
+				aux = aux->esq;
+			else
+				aux = aux->dir;	
+		}
+		aux = ult;
+		if(c < aux->v)
+			aux->esq = novo;
+		else
+			aux->dir = novo;	
+	}
+	
+	//retorna o r
+	return r;
+}
+
+void printa_recursiva(arv *a){
+	if(a != NULL){
+		printa_recursiva(a->esq);
+		printf("%d \n", a->v);
+		printa_recursiva(a->dir);
+	}
+}
