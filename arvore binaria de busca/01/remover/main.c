15def58803993f7d9cf37c565fdc330f53c1d465
William Trindade <williamtrindade777@gmail.com>
2018-09-27 00:31:06 -0300

    Update main.c

--- arvore binaria de busca/01/remover/main.c
+++ arvore binaria de busca/01/remover/main.c
@@ -5,6 +5,7 @@ struct x{
 	int v;
 	struct x *esq;
 	struct x *dir;
+	struct x *ant;
 };
 
 typedef struct x arv;
@@ -21,24 +22,23 @@ main(){
 	r = insere(r, 25);	
 	r = insere(r, 4);	
 	r = insere(r, 12);	
+	
 	printf("\nprinta--------------\n");
 	imprimir(r, 0);
 	
 	printf("\nremove--------------\n");
-	r = remover(r, 20);
+	r = remover(r, 10);
 	
 	printf("\nprinta--------------\n");
 	imprimir(r, 0);
 	
 }
-
-
-
 arv *insere(arv *r, int c){
 	arv *novo = (arv*) malloc(sizeof(arv));
 	novo->v = c;
 	novo->esq = NULL;
 	novo->dir = NULL;
+	novo->ant = NULL;
 	
 	if(r == NULL){
 		r = novo;
@@ -54,8 +54,13 @@ arv *insere(arv *r, int c){
 				aux = aux->dir;	
 		}
 		aux = ult;
-		if(c < aux->v) aux->esq = novo;
-		else aux->dir = novo;	
+		if(c < aux->v) {
+			aux->esq = novo;
+			aux->ant = ult;
+		}else {
+			aux->dir = novo;	
+			aux->ant = ult;
+		}
 	}
 	
 	//retorna o r
@@ -73,26 +78,35 @@ void imprimir(arv *r, int a){
 	}
 }
 
-
-
 arv *remover(arv *r, int x){
 	arv *menorADireita = (arv*) malloc(sizeof(arv));
+	arv *topo = (arv*) malloc(sizeof(arv));
 	
+	topo = r;
+
 	//busca o numero
 	while(r != NULL && x != r->v){
 		if(x > r->v) r = r->dir;
 		if(x < r->v) r = r->esq;
 	}
-	
-	menorADireita = r;
 	menorADireita = r->dir;
-	while(menorADireita->esq != NULL){
-		menorADireita = menorADireita->esq;
-	}
-	menorADireita->esq = r->esq;
 	
-	return r->dir;
-	r->esq = NULL;
-	r->dir = NULL;
-	r->v = 0;
+	if(r == topo){
+		while(menorADireita->esq != NULL){
+			menorADireita = menorADireita->esq;
+		}
+		menorADireita->esq = r->esq;
+		return topo->dir;
+	}else{
+		while(menorADireita->esq != NULL){
+			menorADireita = menorADireita->esq;
+		}
+		menorADireita->esq = r->esq;
+		if(r->ant->dir == r){
+			r->ant->dir = r->dir;
+		}else if(r->ant->esq == r){
+			r->ant->esq = r->dir;
+		}		
+		return topo;
+	}
 }
