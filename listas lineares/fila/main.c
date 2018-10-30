ec57bd8ff6f44bf5139a270527213e5f948788df
William Trindade <williamtrindade777@gmail.com>
2018-10-29 22:31:25 -0300

    verifica se a fila esta vazia

---
+++ listas lineares/fila/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define n 10
+//fifo
+
+main () {
+	
+	int queue[n] = {};
+	
+	printf("%d", isEmpity(queue));
+	
+}
+
+/*
+void remove (int arr[n]) {
+	
+}
+
+void insert (int arr[n], v) {
+	
+}
+
+void size (int arr[n) {
+	
+}
+
+void front (int arr[n) {
+	
+}
+*/
+int isEmpity (int arr[n]) {
+	
+	return (arr[0] == NULL);
+	
+}
+
+/*
+void display () {
+	
+}
+
+*/
