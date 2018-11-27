c86e838a7c37f4e1f44685935b6759716bf56b6c
William Trindade <williamtrindade777@gmail.com>
2018-11-26 23:50:38 -0200

    Update main.c

--- tabela de dispersao/insercao/main.c
+++ tabela de dispersao/insercao/main.c
@@ -1,21 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define tamanho 50
-// tabela hash
-int vet[tamanho] = {};
+#define tamanho 100
 
-int insert(int vet[], int matricula) {
-	vet[matricula%tamanho] = matricula;
+// tabela hash
+int vetor[tamanho] = {};
+
+int calcula(int a, int b) {
 	
-	return matricula%tamanho;
+	return (a%b);
 	
+}
+
+void showInsert(int numero, int posicao) {
+	
+	printf("\nNumero %d inserido na posicao %d\n", numero, posicao);
+	
+}
+
+void showSearch(int numero, int posicao) {
+	
+	printf("\nNumero %d encontrado na posicao %d\n", numero, posicao);
+	
+}
+
+void insert(int vetor[], int numero) {
+	
+	if(numero == 0) {
+		printf("Numero vazio!");
+	}else {
+		vetor[calcula(numero, tamanho)] = numero;
+		showInsert(vetor[calcula(numero, tamanho)], calcula(numero, tamanho));
+	}
+		
+}
+
+int search(int vetor[], int numero) {
+	
+	// return a position of array
+	int ret = calcula(numero, tamanho);	
+	
+	if(vetor[ret] != numero) {
+		printf("Numero não encontrado!");
+	}else {
+		showSearch(vetor[ret], ret);
+		return ret;
+	}
 }
 
 int main() {
 	
-	int x = insert(vet, 201811411);
-	// printf("%d", insert(vet, 201811411));
-	printf("Matricula: %d - Posicao: %d", vet[x], x);
+	insert(vetor, 5);
+	search(vetor, 100);
 	return 0;
 	
 }
